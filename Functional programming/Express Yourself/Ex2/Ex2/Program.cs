281d0e79be0086aaf0fe1e1d2d1fc163237f93b2
Torfand <Torfin@getacademy.no>
2020-12-14 11:16:46 +0100

    x
    x

---
+++ Functional programming/Express Yourself/Ex2/Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace Ex2
+{
+    public sealed class MySingleton
+    {
+        private static MySingleton _instance;
+
+        public MySingleton()
+        {
+        }
+
+        public static MySingleton Instance => _instance ?? (_instance = new MySingleton());
+        public override string ToString() => $"Type Name : {GetType().Name.Split('+')[0]}";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ins1 = MySingleton.Instance;
+            var ins2 = MySingleton.Instance;
+            var referenceEquals = ReferenceEquals(ins1, ins2);
+            Console.WriteLine($"SameObject?,\n {referenceEquals}");
+            Console.WriteLine($"{ins1}");
+        }
+    }
+}
---
+++ Functional programming/Express Yourself/ExpressYourself/ExpressYourself/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LpDumper;
+
+
+namespace ExpressYourself
+{
+    public class DateRange
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public bool DateIsInRange(DateTime checkDate)
+        {
+            return Start.CompareTo(checkDate) <= 0 && End.CompareTo(checkDate) >= 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var testDates =  new List<DateTime>
+           {
+               DateTime.Parse("2015-11-03"),
+               DateTime.Parse("2015-11-01"),
+               DateTime.Parse("2015-10-01"),
+               DateTime.Parse("2015-12-01"),
+               
+           };
+           var range = new DateRange
+           {
+               Start = DateTime.Parse("2015-11-01"),
+               End = DateTime.Parse("2015-11-06")
+           };
+          testDates.ForEach(d=>Console.WriteLine($"{d:yyyy-MM-dd} - {(range.DateIsInRange(d))}"));
+          Console.WriteLine("----------------");
+          range.End = DateTime.MaxValue;
+          testDates.ForEach(d=>Console.WriteLine($"{d:yyyy-MM-dd} - {(range.DateIsInRange(d))}"));
+        }
+    }
+}
